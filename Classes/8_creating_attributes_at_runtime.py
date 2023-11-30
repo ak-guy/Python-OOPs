3c537d9cd78f0bdf5ffdf7ac1db6ffa9fccd7084
Arpit <arpitkumar3203@gmail.com>
2023-11-30 09:15:18 +0530

    added further explaination

--- Classes/8_creating_attributes_at_runtime.py
+++ Classes/8_creating_attributes_at_runtime.py
@@ -62,3 +62,53 @@ p1.say_hello = MethodType(say_hello, p1) # this way we can bind a method say_hel
 print(p1.__dict__.get('say_hello', None))
 print(Person.__dict__.get('say_hello', None))
 p1.say_hello()
+
+'''
+Another way of doing it 
+'''
+
+from types import MethodType
+
+class Person:
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def register_do_work(self, func):
+        '''
+        what this method will do is, it will create an attribute _do_work for object self and assign its functionality based on func
+        '''
+        setattr(self, '_do_work', MethodType(func, self))
+
+    def do_work(self):
+        do_work_method = getattr(self, '_do_work', None)
+
+        if do_work_method:
+            return do_work_method()
+        else:
+            raise AttributeError('You must register do work first')
+        
+good_person = Person('Ankit')
+bad_person = Person('Arpit')
+
+if False:
+    # this will raise AttributeError as _do_work does not belong to good_person obj
+    good_person.do_work()
+
+def take_math(self):
+    print(f'{self.name} is going to take math class today')
+
+print(good_person.__dict__)
+good_person.register_do_work(take_math)
+print(good_person.__dict__)
+
+
+def take_english(self):
+    print(f'{self.name} is going to take english class today')
+
+print(bad_person.__dict__)
+bad_person.register_do_work(take_english)
+print(bad_person.__dict__)
+
+persons = [good_person, bad_person]
+for p in persons:
+    p.do_work()
