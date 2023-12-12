6499cc4422491f9b99cfde05b01fedc2ba77ad4b
ak-guy <arpitkumar3203@gmail.com>
2023-12-12 20:56:07 +0530

    added object class

---
+++ 03_single_inheritance/02_object_class.py
@@ -0,0 +1,8 @@
+'''
+when we define a class that does not inherit from another class explicitly
+then python makes the class inherit from 'object' class implicitly
+'object' is a class of type 'type' 
+
+"class Person:" is same as "class Person(object):"
+
+'''
